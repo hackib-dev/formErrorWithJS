6b1c5e5b60a7640e436a0d99dd963d58a636f31c
hackib-dev <hackibkenn@gmail.com>
2023-01-27 07:57:26 +0100

    FOrm Error with JS

---
+++ static/js/index.js
@@ -0,0 +1,24 @@
+// const form = document.getElementsByClassName("form");
+// const formError = document.getElementsByClassName("error_message");
+
+// let isValid = false;
+
+// function validate_form(){
+
+//     isValid = form[0].checkValidity();
+
+//     if(!isValid){
+//     formError[0].textContent = "Kindly fill out all fields!!!";
+//     }
+//     else {
+//         formError[0].textContent = "Form submitted successfully"
+//     }
+
+// }
+
+// form[0].addEventListener('submit', (e) =>  {
+//     e.preventDefault();
+//     validate_form();
+// })
+
+
